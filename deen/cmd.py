66532ce48aee5f2d33e97541f4a6f803c6d565ce
rashed <x.suwaidi@gmail.com>
2021-07-18 10:08:37 +0400

    pretty print

--- deen/cmd.py
+++ deen/cmd.py
@@ -3,19 +3,22 @@ from telegram.ext import CallbackContext
 from common import utils
 import os
 import requests
+import json
 
 def dua_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
 
     if context.args[0] == 'morning':
-        file = os.path.join(os.path.dirname(__file__), 'resources/dua_morning.txt')
+        file = os.path.join(os.path.dirname(__file__),
+                            'resources/dua_morning.txt')
         duas = utils.get_lines(file)
 
         for dua in duas:
             update.message.reply_text(dua)
 
     if context.args[0] == 'prayer':
-        file = os.path.join(os.path.dirname(__file__), 'resources/dua_prayer.txt')
+        file = os.path.join(os.path.dirname(__file__),
+                            'resources/dua_prayer.txt')
         duas = utils.get_lines(file)
 
         for dua in duas:
@@ -26,10 +29,9 @@ def timing_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
 
     city = 'Dubai'
-    if len(context.args) >0:
-        if context.args[0]=='abudhabi':
+    if len(context.args) > 0:
+        if context.args[0] == 'abudhabi':
             city = 'Abu Dhabi'
-
 
     params = {
         "city": city,
@@ -39,4 +41,6 @@ def timing_command(update: Update, context: CallbackContext) -> None:
     response = requests.get(
         "http://api.aladhan.com/v1/timingsByCity?", params=params)
 
-    update.message.reply_text(response.json()['data']['timings'])
+    data = response.json()['data']['timings']
+
+    update.message.reply_text(json.dumps(data, indent=2))
